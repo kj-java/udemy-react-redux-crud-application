8ec05076441d78ff4041e1d0f892b1a9b2fbf0ec
kj-java <10koji25@gmail.com>
2019-10-14 18:09:32 +0900

    commit conponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+// class App extends Component {
+//   render() {
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange ={() => {console.log("I am changed.")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
 
-function App() {
+const App = () => {
   return(
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange ={() => {console.log("I am changed.")}} />
-    </React.Fragment>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
 }
+
+const Cat = () => 
+{
+  return (
+    <div>Meow!</div>
+  )
+}
+
 export default App;
