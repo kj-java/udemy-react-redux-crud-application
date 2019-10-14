277718ceb178fc5902cfa2ec1aad327841a7feb1
kj-java <10koji25@gmail.com>
2019-10-14 18:34:14 +0900

    commit props

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return(
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange ={() => {console.log("I am changed.")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {name: "noname"}
+  ]
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => 
+const User = (props) => 
 {
   return (
-    <div>Meow!</div>
+    <div>
+      Hi,I am {props.name}, and {props.age} years old!
+    </div>
   )
 }
 
+User.defaultProps = {
+  age: 1
+}
 export default App;
