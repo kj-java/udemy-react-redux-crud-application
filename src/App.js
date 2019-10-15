d31634f0226ead2e7c9db38a5dbfb6bc4b82c8f1
kj-java <10koji25@gmail.com>
2019-10-16 00:28:03 +0900

    react state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import React, {Component} from 'react';
 
-const App = () => {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 5},
-    {name: "Noname", age: 3}
-  ]
-  return(
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => 
+class Counter extends Component
 {
-  return (
-    <div>
-      Hi,I am {props.name}, and {props.age} years old!
-    </div>
-  )
+  constructor(props){
+    super(props)
+    this.state = {count: 0}
+  }
+
+  handlePlasButton = () => {
+    this.setState({count:this.state.count +1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({count:this.state.count -1})
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlasButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+    </React.Fragment>
+    )
+  }
 }
 
-User.propTypes = {
-  name: propTypes.string,
-  age: propTypes.number.isRequired
-}
 export default App;
